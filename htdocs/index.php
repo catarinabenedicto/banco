336298ea6d9f401a1f51c26798626408429dd685
catarinabenedicto <134006596+catarinabenedicto@users.noreply.github.com>
2023-08-22 16:27:54 -0300

    Aula-06/Daniel

---
+++ htdocs/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pt_br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="/css/estilo.css"> 
+
+    <title>Biblioteca Municipal</title>
+
+</head>
+
+<body>
+    
+    <header>
+    
+       <img src="/img/Biblioteca-Banner.png" alt="Biblioteca-Banner">
+       
+       <nav>
+
+            <ul>
+                <li><a href="index.php">Inicio</a></li>
+                <li><a href="usuarios.php">Usuario</a></li>
+                <li><a href="livro.php">Livroa</a></li>
+                <li><a href="#">emprestimo</a></li>
+            </ul>
+
+       </nav>
+
+    </header>
+
+    <main>
+
+
+
+    </main>
+
+</body>
+</html>
